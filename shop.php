f1e4f62296462a9a6ea07a2a5a42d5c2e1d5c783
arbell13 <floresas@students.nu-baliwag.edu.ph>
2023-06-02 09:20:23 +0800

    fuchie

--- shop.php
+++ shop.php
@@ -1,6 +1,6 @@
 <?php
 
-@include 'config.php';
+include 'config.php';
 
 session_start();
 
@@ -21,10 +21,10 @@ if(isset($_POST['add_to_wishlist'])){
    $p_image = $_POST['p_image'];
    $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
 
-   $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+   $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist`");
    $check_wishlist_numbers->execute([$p_name, $user_id]);
 
-   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` ");
    $check_cart_numbers->execute([$p_name, $user_id]);
 
    if($check_wishlist_numbers->rowCount() > 0){
@@ -52,18 +52,18 @@ if(isset($_POST['add_to_cart'])){
    $p_qty = $_POST['p_qty'];
    $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
 
-   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` ");
    $check_cart_numbers->execute([$p_name, $user_id]);
 
    if($check_cart_numbers->rowCount() > 0){
       $message[] = 'already added to cart!';
    }else{
 
-      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` ");
       $check_wishlist_numbers->execute([$p_name, $user_id]);
 
       if($check_wishlist_numbers->rowCount() > 0){
-         $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+         $delete_wishlist = $conn->prepare("DELETE FROM `wishlist`");
          $delete_wishlist->execute([$p_name, $user_id]);
       }
 
