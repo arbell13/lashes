8d08ec7d0ddaf97525d7d20dad23a7c1bd536b61
arbell13 <floresas@students.nu-baliwag.edu.ph>
2023-06-02 09:04:55 +0800

    grr

--- search_page.php
+++ search_page.php
@@ -36,7 +36,7 @@ if(isset($_POST['add_to_wishlist'])){
       $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
       $message[] = 'added to wishlist!';
    }
-
+ 
 }
 
 if(isset($_POST['add_to_cart'])){
